fc6d21b1cb5a6728607dc22076f7b9efbdca1506
LeonMatiasM.A <43658425+LematJump@users.noreply.github.com>
2019-07-01 13:38:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function functionimg1(){
+    document.getElementById('img').src='img1.jpg';
+}
+function functionimg2(){
+    document.getElementById('img').src='img2.jpg';
+}
+function functionimg3(){
+    document.getElementById('img').src='img3.jpg';
+}
+function functionimg4(){
+    document.getElementById('img').src='img4.jpg';
+}
+function functionimg5(){
+    document.getElementById('img').src='img5.jpg';
+}
